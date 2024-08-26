b745370d2fb066885369c3765af70dede6d4c498
cho-subin <100771469+cho-subin@users.noreply.github.com>
2024-08-26 23:05:53 +0900

    :lipstick: css: rootLayout css, header 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+import Header from "@/components/common/header/Header";
 
 export const metadata = {
   title: "Clip",
@@ -13,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <body>{children}</body>
+      <body className="bg-backgroundColor max-w-[420px] mx-auto relative h-screen">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }

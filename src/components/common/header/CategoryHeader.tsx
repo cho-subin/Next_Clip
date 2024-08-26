0eccf3e4332019b328fa1e363f300ecb50118f5c
cho-subin <100771469+cho-subin@users.noreply.github.com>
2024-08-26 23:09:42 +0900

    :lipstick: categoryHeader css 추가

--- src/components/common/header/CategoryHeader.tsx
+++ src/components/common/header/CategoryHeader.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import Link from "next/link";
 import React from "react";
 
 const CategoryHeader = () => {
-  return <div></div>;
+  return (
+    <nav
+      className="py-0 px-5 flex flex-row justify-around w-full h-[46px]
+      leading-[46px] bg-black text-green border-b border-white"
+    >
+      <Link href={"/"}>Home</Link>
+      <Link href={"/category"}>Category</Link>
+      <Link href={"#"}>Product</Link>
+    </nav>
+  );
 };
 
 export default CategoryHeader;
